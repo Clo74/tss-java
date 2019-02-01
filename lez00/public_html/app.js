7f597866028f2df9917109a9e64d945b2c20c44b
tss <alfonso.domenici.it>
2019-02-01 16:46:44 +0100

    modifiche

--- lez00/public_html/app.js
+++ lez00/public_html/app.js
@@ -1,16 +1,25 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+import Dado from './Dado.js';
+import Giocatore from './Giocatore.js';
 
-var msg = 'ciao da var';
 
-let msg1 = 'ciao da let';
+//main
 
-function f1() {
-    let x = 10;
-    console.log('stampa x in f1 -> ' + x);
+let giocatore = new Giocatore(10,'Pippo');
+let dGiocatore = new Dado();
+let dBanco = new Dado();
+while(giocatore.soldi>0){
+    let puntiGiocatore = dGiocatore.lancio();
+    let puntiBanco = dBanco.lancio();
+    if(puntiGiocatore>puntiBanco){
+        giocatore.incrementa();
+    }else{
+        giocatore.decrementa();
+    }
+    stampaMano(puntiGiocatore,puntiBanco,giocatore.soldi);
 }
 
-    
+function stampaMano(puntiGiocatore,puntiBanco,soldi){
+    console.log('--------------------------------------------------------------');
+    console.log(`punti giocatore -> ${puntiGiocatore} , punti banco -> ${puntiBanco}`);
+    console.log(`soldi giocatore -> ${soldi}`);
+}
